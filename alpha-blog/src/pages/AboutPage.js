21451cba5f38344b288e38b54ff20907b311f3d0
JudahSan <baraka.judah@yahoo.com>
2023-01-07 17:38:38 +0300

    created and linked the articles list, made article list modular, created a 404 page

--- alpha-blog/src/pages/AboutPage.js
+++ alpha-blog/src/pages/AboutPage.js
@@ -1,8 +1,29 @@
 const AboutPage = () => {
   return (
-    <h1>
-      This is the about page!
+
+    <div>
+          <h1>
+      I was there when it was written
     </h1>
+
+    <p>
+    Hello and welcome to my blog! My name is Judahsan and I'm the creator of this space. 
+    I'm a full-stack web developer with a background in data science, and I've always been 
+    passionate about using technology to create and solve problems.
+    </p>
+    <p>In my free time, I enjoy 
+    bingewatching One Piece and Modern Family, going to the orchestra, and sharing memes with 
+    my friends. As a developer, my ultimate goal is to create a multi platform RPG game. 
+    </p>
+    <p>
+    I started this blog as a way to share my thoughts, experiences, and knowledge with others, 
+    and I hope you find something here that interests you. Thank you for visiting and I hope 
+    you have a great time exploring my blog. If you have any questions or just want to say hello, 
+    don't hesitate to reach out!
+    </p>
+    </div>
+
+    
   );
 }
 
--- alpha-blog/src/pages/ArticlesListPage.js
+++ alpha-blog/src/pages/ArticlesListPage.js
@@ -1,22 +1,14 @@
 import articles from './article-content';
-import { Link } from 'react-router-dom';
+import ArticlesList from '../components/ArticlesList';
 
 const ArticlesListPage = () => {
   return (
     <>
-        <h1>
+    <h1>
       Articles
     </h1>
-    {articles.map(articles => (
-      <Link key={articles.name} className='article-list-item' to={`/articles/${articles.name}`}>
-        <h3>
-          {articles.title}
-        </h3>
-        <p>
-          {articles.content[0].substring(0, 150)}....
-        </p>
-      </Link>
-    )) }</>
+    <ArticlesList articles={articles} />
+    </>
   );
 }
 
