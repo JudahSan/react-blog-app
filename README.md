ec100a3fe9eaca36d22115e49748cd50dd08f484
JudahSan <baraka.judah@yahoo.com>
2023-01-08 00:43:21 +0300

    authentication with firebase

--- README.md
+++ README.md
@@ -7,3 +7,5 @@
     ````
 
 - Add `"proxy": "http://localhost:8000/"` in `package.json` file to connect front-end and back-end
+
+- Firebase
--- alpha-blog/src/index.js
+++ alpha-blog/src/index.js
@@ -3,6 +3,29 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDfjYQFM4X1yyKMNG7fWjo84qozNCWziv4",
+  authDomain: "react-alpha-blog.firebaseapp.com",
+  projectId: "react-alpha-blog",
+  storageBucket: "react-alpha-blog.appspot.com",
+  messagingSenderId: "531734009235",
+  appId: "1:531734009235:web:c6af5435399d3fa9178466",
+  measurementId: "G-VXVYJ6T1VM"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
